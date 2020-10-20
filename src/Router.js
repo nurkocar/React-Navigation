fe409be8f5ae23d2be3ac1f761b03b72a8a1ac69
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-20 21:04:02 +0000

    completed navigation between two pages

--- src/Router.js
+++ src/Router.js
@@ -5,17 +5,32 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import First from './pages/First';
+import Second from './pages/Second';
 
 const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+function Router() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    gestureEnabled: true
+                }}
+            >
+                <Stack.Screen
+                    name="FirstPage"
+                    component={First}
+                />
+                <Stack.Screen
+                    name="SecondPage"
+                    component={Second}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-export default App;
+export default Router;
--- src/pages/Second.js
+++ src/pages/Second.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 
 
 const Second = (props) => {
 
+    const userValue = props.route.params.selectedValue
+
+    const userNumber = props.route.params.myNumber;
+
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Text>Second</Text>
+        <SafeAreaView>
+            <View>
+                <Text style={{ fontSize: 40 }}>Second</Text>
+                <Text style={{ fontSize: 40 }}>value: {userValue} {userNumber}</Text>
+                <Button
+                    title="Go back"
+                    onPress={() => props.navigation.goBack()}
+                />
+            </View>
         </SafeAreaView>
-    )
+    );
 }
 
 export default Second;
