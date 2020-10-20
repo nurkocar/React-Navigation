b8b9b5475686813272f8b5a193e1726f0e256eba
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-20 19:26:02 +0000

    created two pages

---
+++ src/pages/First.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+
+const First = (props) => {
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>First</Text>
+        </SafeAreaView>
+    )
+}
+
+export default First;
---
+++ src/pages/Second.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+
+const Second = (props) => {
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>Second</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Second;
